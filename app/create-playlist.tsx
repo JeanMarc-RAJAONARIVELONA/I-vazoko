c0adc921a7194e9ae0bf9be8a4ff96bab06549ac
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2025-03-16 10:24:33 +0300

    feat: create playlist

--- app/create-playlist.tsx
+++ app/create-playlist.tsx
@@ -1,7 +1,33 @@
 import CreatePlaylist from "@/component/CreatePlaylist";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { useTheme } from "@/context/ThemeContext";
 
 const CreatePlaylistRoute: React.FC = () => {
-  return <CreatePlaylist />;
+  const { mode, playlistId } = useLocalSearchParams();
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: mode === "add" ? "Ajouter des titres" : "Créer une Playlist",
+          presentation: "modal",
+          headerShown: true,
+          animation: "slide_from_bottom",
+          headerStyle: {
+            backgroundColor: theme.cardBg,
+          },
+          headerTitleStyle: {
+            color: theme.text,
+          },
+        }}
+      />
+      <CreatePlaylist
+        mode={mode as "create" | "add"}
+        playlistId={playlistId as string}
+      />
+    </>
+  );
 };
 
 export default CreatePlaylistRoute;
